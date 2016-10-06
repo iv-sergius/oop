badc9915e27fdaf8d31868be22109ba8eef1d913
Sergey Ivanov <iv.sergius@yandex.ru>
2016-10-06 17:20:40 +0300

    Add radix programm

---
+++ lab1/radix/radix.cpp
@@ -0,0 +1,146 @@
+﻿#include <iostream>
+#include <string>
+#include <limits>
+#include <stdio.h>
+
+#define CONVERT_NO_ERROR 0
+#define CONVERT_ERROR_EMPTY_STRING 1
+#define CONVERT_ERROR_LONG_OVERFLOW 2
+#define CONVERT_ERROR_LETTER_OUT_RANGE 3
+#define CONVERT_ERROR_DIGIT_OUT_RANGE 4
+using namespace std;
+
+
+
+char DigitToLetter(long digit, int radix, int & wasError)
+{
+	wasError = CONVERT_NO_ERROR;
+	if (digit >= 0 && digit <= 9)
+	{
+		return (char)('0' + digit);
+	}
+	if (digit < radix && digit >= 10)
+	{
+		return (char)('A' - 10  + digit);
+	}
+	wasError = CONVERT_ERROR_DIGIT_OUT_RANGE;
+	return '\0';
+}
+
+long LetterToDigit(char letter, int radix, int & wasError)
+{
+	wasError = CONVERT_NO_ERROR;
+	if (letter >= '0' && letter <= '9')
+	{
+		return (long)(letter - '0');
+	}
+	if (letter >= 'A' && letter < 'A' - 10 + radix)
+	{
+		return (long)(letter - 'A' + 10);
+	}
+	wasError = CONVERT_ERROR_LETTER_OUT_RANGE;
+	return 0;
+}
+
+long StringToLong(const string str, int radix, int & wasError)
+{
+	wasError = CONVERT_NO_ERROR;
+	long resultLong = 0;
+	if (!str.length())
+	{
+		wasError = CONVERT_ERROR_EMPTY_STRING;
+		return 0;
+	}
+	int firstDigitPosition = (int)(str[0] == '-');
+	int sign = 1 - 2 * firstDigitPosition;
+	int i = firstDigitPosition;
+	while ( (i < str.length()))
+	{
+		int digit = LetterToDigit(str[i++], radix, wasError);
+		// check on limits
+		resultLong = radix * resultLong + digit;
+	}
+	resultLong = sign * resultLong;
+	return resultLong;
+}
+
+string LongToString(const long number, int radix, int & wasError)
+{
+	wasError = CONVERT_NO_ERROR;
+	string resultStr = "";
+	long numberLocal = number;
+	int innerError = CONVERT_NO_ERROR;
+	
+	bool isNegative = numberLocal < 0; // save sing of numbers
+	long rest = abs(numberLocal % radix); 
+	numberLocal = abs(numberLocal / radix);
+	while (rest || numberLocal)
+//	while (rest > 0 && numberLocal > 0)
+	{
+		innerError = CONVERT_NO_ERROR;
+		char nextCh = DigitToLetter(rest, radix, innerError);
+		if (innerError != CONVERT_NO_ERROR)
+		{
+			wasError = innerError;
+			return "";
+		}
+		resultStr = nextCh + resultStr;
+		rest = numberLocal % radix;
+		numberLocal = numberLocal / radix;
+	}
+	if (isNegative) resultStr = '-' + resultStr;
+	if (!number)
+	{
+		resultStr = "0";
+	}
+	return resultStr;
+}
+
+int main(int argv, char *argc[])
+{
+	int baseFrom, baseTo;
+	if (argv != 4)
+	{
+		cout << "Invalid arguments amount\n"
+			<< "Usage radix.exe <source notation> <destination notation> <value>\n";
+		return 1;
+	}
+	int wasError = CONVERT_NO_ERROR;
+	
+	baseFrom = StringToLong(argc[2], 10, wasError);
+	if (wasError != CONVERT_NO_ERROR)
+	{
+		cout << endl;
+		return wasError;
+	}
+	baseTo = StringToLong(argc[3], 10, wasError);
+	if (wasError != CONVERT_NO_ERROR)
+	{
+		cout << endl;
+		return wasError;
+	}
+	long internLong = StringToLong(argc[1], baseFrom, wasError);
+	if (wasError != CONVERT_NO_ERROR)
+	{
+		cout << endl;
+		return wasError;
+	}
+	string resultStr = LongToString(internLong, baseTo, wasError);
+	if (wasError != CONVERT_NO_ERROR)
+	{
+		cout << endl;
+		return wasError;
+	}
+	//cout << internLong << ' ' << resultStr << endl;
+	cout << resultStr << endl;
+	//wasError;
+	//cout << StringToLong(argc[1], 10, wasError) << ' ' << StringToLong(argc[2], 10, wasError) << endl;
+	//cout << LongToString(-255, 16, wasError) << endl;
+	//cout << LongToString(256, 16, wasError) << endl;
+	//cout << LongToString(255, 16, wasError) << endl;
+	//cout << -10 % 3 << endl;
+	//cout << -10 / 3 << endl;
+	//std::cout << "Minimum value for long: " << std::numeric_limits<long>::min() << '\n';
+	//std::cout << "Maximum value for long: " << std::numeric_limits<long>::max() << '\n';
+	return 0;
+}
---
+++ lab1/radix/test.bat
@@ -0,0 +1,42 @@
+set PROGRAM="%~1"
+
+%PROGRAM% 255 10 16 > %TEMP%\test.txt
+if ERRORLEVEL 1 goto errRun
+fc.exe test\test255_10_16.txt %TEMP%\test.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% 256 10 16 > %TEMP%\test.txt
+if ERRORLEVEL 1 goto errRun
+fc.exe test\test256_10_16.txt %TEMP%\test.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% 0 10 16 > %TEMP%\test.txt
+if ERRORLEVEL 1 goto errRun
+fc.exe test\test0_10_16.txt %TEMP%\test.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% -255 10 16 > %TEMP%\test.txt
+if ERRORLEVEL 1 goto errRun
+fc.exe test\test-255_10_16.txt %TEMP%\test.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% FF 16 10 > %TEMP%\test.txt
+if ERRORLEVEL 1 goto errRun
+fc.exe test\testFF_16_10.txt %TEMP%\test.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% -100 16 10 > %TEMP%\test.txt
+if ERRORLEVEL 1 goto errRun
+fc.exe test\test-100_16_10.txt %TEMP%\test.txt
+if ERRORLEVEL 1 goto err
+
+echo Program testing succcesed
+exit 0
+
+:errRun
+echo echo Program testing failed : Program exit with error
+exit 1
+
+:err
+echo Program testing failed : Incorrect result
+exit 1
