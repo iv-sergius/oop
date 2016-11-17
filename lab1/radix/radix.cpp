d6ac12b2ff3d7780d31144f5991fd2a90f754b7a
Sergey Ivanov <iv.sergius@yandex.ru>
2016-11-17 19:45:51 +0300

    Fix all issues in radix

--- lab1/radix/radix.cpp
+++ lab1/radix/radix.cpp
@@ -15,7 +15,7 @@ enum error {
 using namespace std;
 
 
-char DigitToLetter(long digit, int radix, int & errorCode)
+char DigitToLetter(long digit, int radix, error & errorCode)
 {
 	errorCode = CONVERT_NO_ERROR;
 	if (digit >= 0 && digit <= 9)
@@ -30,7 +30,7 @@ char DigitToLetter(long digit, int radix, int & errorCode)
 	return '\0';
 }
 
-long LetterToDigit(char letter, int radix, int & errorCode)
+long LetterToDigit(char letter, int radix, error & errorCode)
 {
 	errorCode = CONVERT_NO_ERROR;
 	if (letter >= '0' && letter <= '9')
@@ -41,11 +41,15 @@ long LetterToDigit(char letter, int radix, int & errorCode)
 	{
 		return (long)(letter - 'A' + 10);
 	}
+	if (letter >= 'a' && letter < 'a' - 10 + radix)
+	{
+		return (long)(letter - 'a' + 10);
+	}
 	errorCode = CONVERT_ERROR_LETTER_OUT_RANGE;
 	return 0;
 }
 
-long StringToLong(const string str, int radix, int & errorCode)
+long StringToLong(const string & str, int radix, error & errorCode)
 {
 	errorCode = CONVERT_NO_ERROR;
 	unsigned long resultLong = 0;
@@ -54,12 +58,11 @@ long StringToLong(const string str, int radix, int & errorCode)
 		errorCode = CONVERT_ERROR_EMPTY_STRING;
 		return 0;
 	}
-	unsigned int firstDigitPosition = (int)(str[0] == '-');
-	long sign = 1 - 2 * firstDigitPosition;
-	unsigned int i = firstDigitPosition;
-	while ((i < str.length()))
+	unsigned int firstDigitPosition = (str[0] == '+' || str[0] == '-') ? 1 : 0;
+	long sign = (str[0] == '-') ? -1 : 1;
+	for (size_t i = firstDigitPosition; i < str.length(); ++i)
 	{
-		unsigned int digit = LetterToDigit(str[i++], radix, errorCode);
+		unsigned int digit = LetterToDigit(str[i], radix, errorCode);
 		// check on limits
 		if (resultLong <= (LONG_MAX - digit + firstDigitPosition) / radix)
 		{
@@ -74,14 +77,28 @@ long StringToLong(const string str, int radix, int & errorCode)
 	return (long) sign * resultLong;
 }
 
-string LongToString(const long number, int radix, int & errorCode)
+string LongToString(const long number, int radix, error & errorCode)
 {
 	errorCode = CONVERT_NO_ERROR;
 	string resultStr = "";
-	bool isNegative = number < 0; // save sing of numbers
+	bool isNegative = number < 0; // save sign of numbers
 	unsigned long numberLocal = abs(number);
-	int innerError = CONVERT_NO_ERROR;
+	error innerError = CONVERT_NO_ERROR;
 	
+	unsigned long remainder;
+	do
+	{
+		remainder = numberLocal % radix;
+		numberLocal = numberLocal / radix;
+		char nextCh = DigitToLetter(remainder, radix, innerError);
+		if (innerError != CONVERT_NO_ERROR)
+		{
+			errorCode = innerError;
+			return "";
+		}
+		resultStr.push_back(nextCh);
+	} while (numberLocal > 0);
+/*
 	unsigned long rest = numberLocal % radix; 
 	numberLocal = numberLocal / radix;
 	while (rest || numberLocal)
@@ -94,30 +111,26 @@ string LongToString(const long number, int radix, int & errorCode)
 			errorCode = innerError;
 			return "";
 		}
-		resultStr = nextCh + resultStr;
+		resultStr.push_back(nextCh);
 		rest = numberLocal % radix;
 		numberLocal = numberLocal / radix;
 	}
-	if (isNegative) resultStr = '-' + resultStr;
+*/
+	if (isNegative) resultStr.push_back('-');
 	if (!number)
 	{
 		resultStr = "0";
 	}
+	std::reverse(resultStr.begin(), resultStr.end());
 	return resultStr;
 }
 
-int main(int argv, char *argc[])
+error radix(const std::string &sourceNotation, const std::string &destinationNotation, const std::string &value, std::string & resultValue)
 {
+	resultValue = "";
+	error errorCode = CONVERT_NO_ERROR;
 	int baseFrom, baseTo;
-	if (argv != 4)
-	{
-		cout << "Invalid arguments amount\n"
-			<< "Usage radix.exe <source notation> <destination notation> <value>\n";
-		return 1;
-	}
-	int errorCode = CONVERT_NO_ERROR;
-	
-	baseFrom = StringToLong(argc[2], 10, errorCode);
+	baseFrom = StringToLong(sourceNotation, 10, errorCode);
 	if (errorCode != CONVERT_NO_ERROR)
 	{
 		cout << endl;
@@ -128,7 +141,7 @@ int main(int argv, char *argc[])
 		cout << endl;
 		return CONVERT_ERROR_INCORRECT_BASE;
 	}
-	baseTo = StringToLong(argc[3], 10, errorCode);
+	baseTo = StringToLong(destinationNotation, 10, errorCode);
 	if (errorCode != CONVERT_NO_ERROR)
 	{
 		cout << endl;
@@ -139,18 +152,31 @@ int main(int argv, char *argc[])
 		cout << endl;
 		return CONVERT_ERROR_INCORRECT_BASE;
 	}
-	long internLong = StringToLong(argc[1], baseFrom, errorCode);
+	long internLong = StringToLong(value, baseFrom, errorCode);
 	if (errorCode != CONVERT_NO_ERROR)
 	{
 		cout << endl;
 		return errorCode;
 	}
-	string resultStr = LongToString(internLong, baseTo, errorCode);
+	resultValue = LongToString(internLong, baseTo, errorCode);
 	if (errorCode != CONVERT_NO_ERROR)
 	{
 		cout << endl;
 		return errorCode;
 	}
-	cout << resultStr << endl;
-	return 0;
+	return errorCode;
+}
+
+int main(int argv, char *argc[])
+{
+	if (argv != 4)
+	{
+		cout << "Invalid arguments amount\n"
+			<< "Usage radix.exe <source notation> <destination notation> <value>\n";
+		return 1;
+	}
+	std::string resultValue;
+	error errorCode = radix(argc[1], argc[2], argc[3], resultValue);
+	std::cout << resultValue << endl;
+	return errorCode;
 }
--- lab1/radix/test.bat
+++ lab1/radix/test.bat
@@ -1,52 +1,52 @@
 set PROGRAM="%~1"
 
-%PROGRAM% 255 10 16 > %TEMP%\test.txt
+%PROGRAM% 10 16 255 > %TEMP%\test.txt
 if ERRORLEVEL 1 goto errRun
 fc.exe test\test255_10_16.txt %TEMP%\test.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% 256 10 16 > %TEMP%\test.txt
+%PROGRAM% 10 16 +256 > %TEMP%\test.txt
 if ERRORLEVEL 1 goto errRun
 fc.exe test\test256_10_16.txt %TEMP%\test.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% 0 10 16 > %TEMP%\test.txt
+%PROGRAM% 10 16 +0> %TEMP%\test.txt
 if ERRORLEVEL 1 goto errRun
 fc.exe test\test0_10_16.txt %TEMP%\test.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% -255 10 16 > %TEMP%\test.txt
+%PROGRAM% 10 16 -255 > %TEMP%\test.txt
 if ERRORLEVEL 1 goto errRun
 fc.exe test\test-255_10_16.txt %TEMP%\test.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% FF 16 10 > %TEMP%\test.txt
+%PROGRAM% 16 10 +fF > %TEMP%\test.txt
 if ERRORLEVEL 1 goto errRun
 fc.exe test\testFF_16_10.txt %TEMP%\test.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% -100 16 10 > %TEMP%\test.txt
+%PROGRAM% 16 10 -100 > %TEMP%\test.txt
 if ERRORLEVEL 1 goto errRun
 fc.exe test\test-100_16_10.txt %TEMP%\test.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% 2147483647 10 2 > %TEMP%\test.txt
+%PROGRAM% 10 2 2147483647 > %TEMP%\test.txt
 if ERRORLEVEL 1 goto errRun
 fc.exe test\test2147483647_10_2.txt %TEMP%\test.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% -2147483648 10 2 > %TEMP%\test.txt
+%PROGRAM% 10 2 -2147483648 > %TEMP%\test.txt
 if ERRORLEVEL 1 goto errRun
 fc.exe test\test-2147483648_10_2.txt %TEMP%\test.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% -2147483649 10 2 > %TEMP%\test.txt
+%PROGRAM% -10 2 2147483649 > %TEMP%\test.txt
 if not ERRORLEVEL 1 goto errRun
 
-%PROGRAM% -214748A 10 2 > %TEMP%\test.txt
+%PROGRAM% 10 2 -214748A > %TEMP%\test.txt
 if not ERRORLEVEL 1 goto errRun
 
-%PROGRAM% -214748A 1 2 > %TEMP%\test.txt
+%PROGRAM% 1 2 -214748A > %TEMP%\test.txt
 if not ERRORLEVEL 1 goto errRun
 
 
